533bc2f128ffbf2535e82b7ed6d69d245f432ebc
total3dx <total3dx@gmail.com>
2025-08-30 16:52:11 -0400

    expanded cards

--- src/jsx/components/ui/AtBoard.jsx
+++ src/jsx/components/ui/AtBoard.jsx
@@ -40,7 +40,12 @@ export default class AtBoard extends React.Component {
 			{ 
 				id: '1', 
 				position: { x: 400, y: 400 }, 
-				data: { click : () => { console.log("click");} },
+				data: { 
+					click : () => { console.log("click");},
+					title : "Node Example",
+					description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap ",
+					date : "1d ago"
+				 },
 				type: 'simepleCard'
 			},
 			{ 
--- src/jsx/components/ui/AtCard.jsx
+++ src/jsx/components/ui/AtCard.jsx
@@ -21,25 +21,64 @@ import ReactFlow, {
 export default class AtCard extends React.Component {
 	timer = null
 	container = React.createRef()
+	parentNode = null
 	state = {
 		loading : false,
-		selected : false
+		selected : false,
+		overflow :false
 	}
 
 	constructor(props) {
 		super(props);
 		console.log(props);
 	}
-	componentDidMount() {}
+	componentDidMount() {
+		this.parentNode = $(this.container.current).parent();
+		this.onResize();
+	}
 	componentDidUpdate() {}
 
-		
-	showLoading = () => {
-		this.setState({ loading: true });
+	onResize = (e) => {
+		// Trunc
+		let currentDOM = $(this.container.current);
+		let descDOM = currentDOM.find(".at__card__content");
+		let textDOM = descDOM.find("p");
+
+		let isCurrentOverflow = false;
+		if (descDOM.outerHeight() < textDOM.outerHeight()) {
+			isCurrentOverflow = true;
+		}
+
+		if (this.state.overflow != isCurrentOverflow) this.setState({ overflow : isCurrentOverflow });
+
+
+
+		return;
+		/*
+		let currentDOM = $(this.container.current);
+		let textDOM = currentDOM.find("p");
+		textDOM.text(this.props.data.description);
+		var divh = currentDOM.height();
+		while (textDOM.outerHeight() > Math.max(divh-23, 23)) {
+		    textDOM.text(function (index, text) {
+		    	let txt = text.replace(/\W*\s(\S)*$/, '...');
+		        return txt;
+		    });
+		}*/
 	}
 
-	hideLoading = () => {
-		this.setState({ loading: false });
+	expand = (e) => {
+		this.parentNode.css({ height : this.getExpandedHight() + "px" });
+		this.onResize();
+	}
+
+	getExpandedHight = (e) => {
+		// Trunc
+		let currentDOM = $(this.container.current);
+		let descDOM = currentDOM.find(".at__card__content");
+		let textDOM = descDOM.find("p");
+		let delta = textDOM.outerHeight() - descDOM.outerHeight() + 23;
+		return (this.parentNode.outerHeight() + delta);
 	}
 
 	
@@ -48,7 +87,8 @@ export default class AtCard extends React.Component {
 
 		const classnames = classNames({
 			"at__card" : true,
-			"at__card--active" : this.state.loaded
+			"at__card--active" : this.state.loaded,
+			"at__card--overflow" : this.state.overflow
 		})
 
 		return (
@@ -56,13 +96,13 @@ export default class AtCard extends React.Component {
 				<div className="at__card__content-container" >
 					<div className="at__card__label">
 						<div className="at__card__ico">ICO</div>
-						<div className="at__card__title">Title</div>
+						<div className="at__card__title">{ this.props.data.title }</div>
 					</div>
-					<div className="at__card__content">
-						lalalalala
-					</div>
+					<div className="at__card__content"><p>{ this.props.data.description }</p></div>
+					<div className="at__card__content-overflow" onClick={this.expand}>...</div>
 				</div>
-				<NodeResizer minWidth={100} minHeight={30} />
+				<div className="at__card__date">{ this.props.data.date }</div>
+				<NodeResizer minWidth={100} minHeight={30} onResize={this.onResize}  />
 				<Handle type="target" position="left" />
 				<Handle type="source" position="right" />
 			</div>
