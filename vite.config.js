e751aba7a06dacba61c831cfe02d44cd1a7b34ce
total3dx <total3dx@gmail.com>
2025-08-31 01:19:16 -0400

    removing assets from css

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,28 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({svgrOptions: {}})],
+  plugins: [
+    react(), 
+    svgr({svgrOptions: {}}),
+    //replace assets from css
+    {
+      name: 'rewrite-css-asset-paths',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          if (fileName.endsWith('.css')) {
+            const file = bundle[fileName];
+            file.source = file.source.replace(/\.\/assets\/images\//g, './images/');
+          }
+        }
+      },
+    }
+  ],
   base: '/demo/finam/',
   sass: {
     api: 'modern-compiler',
+  },
+  build: {
+    assetsDir: '', // Remove 'assets' folder from URLs
   },
   css: {
     preprocessorOptions: {
